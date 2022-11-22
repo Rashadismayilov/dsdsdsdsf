8b02baaaf933ceff64d288ca1a90ec753a62dbb5
Rashadismayilov <112488764+Rashadismayilov@users.noreply.github.com>
2022-11-22 18:52:33 +0400

    Add files via upload

---
+++ homewok 2/js/main.js
@@ -0,0 +1,16 @@
+//Task2
+
+// function myFunction(){
+//     let person = prompt('Please enter your age')
+//     if(person<18){
+//        alert('Yasin catmir') 
+//     }else
+//     {}
+// }
+
+//Task1
+
+// function myFunction() {
+//     confirm("Press a button!")   
+//     document.getElementById("demo").style.display = 'block'
+//   }
